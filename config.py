95e871986ec3a653d70c4e172e8e8a8bb5cbbfef
Sharon Tinta <sharon.tinta@sbssecurity.io>
2023-08-25 08:42:51 -0400

    put text align left

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import configparser
 TEXT_TITLE_INDEX = "WebApp-Demo"
 TEXT_EMPTY_PAGE = ["Hello World","To see the information, please deploy your app"]
 TEXT_NAME_ITEM_MENU = ["List","Charts"]
-TEXT_TITLE_CONTENT_TABLE = "List of clients"
+TEXT_TITLE_CONTENT_TABLE = "ORDERS TAKEN"
 TEXT_TITLE_CONTENT_CHART = "Charts"
 
 config = configparser.ConfigParser()
